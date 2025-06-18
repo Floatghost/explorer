651ad6f57de53082b5e43fb8691c999463690b93
Floatghost <levin.liechti@hotmail.com>
2025-06-18 09:41:10 +0200

    working on preview basname behaving strange

--- src/lib/components/preview/preview.svelte
+++ src/lib/components/preview/preview.svelte
@@ -1,11 +1,36 @@
 <script lang="ts">
+    import { onMount } from 'svelte';
+    import { convertFileSrc } from '@tauri-apps/api/core';
+    import { basename } from '@tauri-apps/api/path';
+
     export let selectedFiles: { selected: boolean, path: string }[];
+
+    let img_path: string = convertFileSrc("C:\\Users\\sus.levin.liechti\\Downloads\\happy-tree.png");
+    let name: string = "";
+
+    $: if (selectedFiles.length === 1) {
+        img_path = convertFileSrc(selectedFiles[0].path);
+        updateName(selectedFiles[0].path);
+    }
+
+    async function updateName(path: string) {
+        name = await basename(path);
+    }
+
+    // optionally initialize on mount
+    onMount(() => {
+        if (selectedFiles.length === 1) {
+            img_path = convertFileSrc(selectedFiles[0].path);
+        }
+        updateName(selectedFiles[0].path);
+    });
 </script>
 
 <div class="preview-wrapper">
     <div class="name">
-        
+        {name}
     </div>
+    <img src={img_path}>
 </div>
 
 <style>
